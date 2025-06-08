34defbcb8f8956dbea3ffef445fc9e8f601c4341
sharon <ali93456@gmail.com>
2025-06-08 04:02:44 +0300

    feat: initialize database in main function

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/sharon-xa/gomux/cmd"
+import (
+	"github.com/sharon-xa/gomux/cmd"
+	"github.com/sharon-xa/gomux/internal/database"
+)
 
 func main() {
+	database.InitDB()
 	cmd.Execute()
 }
